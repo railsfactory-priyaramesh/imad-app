0fb5a54181b90703ce20203e9c8ac42aaa24718b
railsfactory-priyaramesh <priyaramesh@railsfactory.org>
2017-08-08 11:35:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/Artical one', function(req,res){
+app.get('/Artical one', function(req,res) {
     res.send('Artical one requested and will be served here');
     });
 app.get('/Artical two', function(req,res){
